6a1cd55656e7eaa195530897b16efc7b9d63d035
eidancodez <eidankhan.student.muet@gmail.com>
2022-10-12 12:28:11 +0500

    Comments were added to explain the steps

--- implementations/LinkedList.java
+++ implementations/LinkedList.java
@@ -19,39 +19,41 @@ public class LinkedList{
         Node node = new Node();
         node.data = data;
         node.nextNode = null;
-        if(head == null){
+        if(head == null){       // if there are no nodes then makes this node as head node
             head = node;
         }
         else{
             Node n = head;
-            while(n.nextNode != null){
-                n = n.nextNode;
+            while(n.nextNode != null){      // iterate over whole list untill next node  
+                n = n.nextNode;             // doesn't become null
 
             }
-            n.nextNode = node;
+            n.nextNode = node;              //  Make node to be added as a next node
         }
     }
 
+    // Adding node at the start of LinkedList
     public void addAtStart(int data){
         Node node = new Node();
         node.data = data;
-        node.nextNode = head;
-        head = node;
+        node.nextNode = head; // shuffles head node to next node
+        head = node;          // Node to be added becomes the head node
     }
 
+    // Adding node at a particular position
     public void addAt(int index, int data){
         Node node = new Node();
         node.data = data;
         node.nextNode = null;
 
-        if(index==0)   
-            addAtStart(data);
+        if(index==0)                // when there are no nodes 
+            addAtStart(data);       // then addStart() adds node at the start
 
         Node n = head;
-        for(int i=0; i<index-1; i++){
+        for(int i=0; i<index-1; i++){   // iterate all nodes which are before a node at (index-1)
             n = n.nextNode;
         }
-        node.nextNode = n.nextNode; //
+        node.nextNode = n.nextNode; //  Node with index 'i' becomes next node
         n.nextNode = node;
     }
 
